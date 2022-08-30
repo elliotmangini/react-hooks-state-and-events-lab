a0b7e99858c469f536869525365950247980663e
Elliot Mangini <elliot.mangini@gmail.com>
2022-08-30 16:46:06 -0600

    I FREAKING DID IT

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [ isInCart, setisInCart ] = useState(false);
+
+  function handleCart () {
+    setisInCart(isInCart => !isInCart);
+  }
+
+  const determineClass = isInCart ? "in-cart" : "";
+
+
   return (
-    <li className="">
+    <li className={determineClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{!isInCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [ queryCategory, setQueryCategory ] = useState("All")
+
+  function updateCategory (e) {
+    setQueryCategory(e.target.value);
+  }
+
+  const itemsToDisplay = items.filter(item => {
+    if (queryCategory === "All") return true;
+    return item.category === queryCategory;
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={updateCategory} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
